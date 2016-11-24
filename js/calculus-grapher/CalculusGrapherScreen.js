47ae37b957de12ba3b1ca4673ae99fee5895526c
Jonathan Olson <jonathan.olson@colorado.edu>
2016-11-23 22:22:47 -0700

    Converting backgroundColor => backgroundColorProperty, see https://github.com/phetsims/joist/issues/373

--- js/calculus-grapher/CalculusGrapherScreen.js
+++ js/calculus-grapher/CalculusGrapherScreen.js
@@ -13,6 +13,8 @@ define( function( require ) {
   var CalculusGrapherScreenView = require( 'CALCULUS_GRAPHER/calculus-grapher/view/CalculusGrapherScreenView' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Screen = require( 'JOIST/Screen' );
+  var Property = require( 'AXON/Property' );
+  var Color = require( 'SCENERY/util/Color' );
 
   /**
    * @constructor
@@ -21,7 +23,7 @@ define( function( require ) {
     Screen.call( this,
       function() { return new CalculusGrapherModel(); },
       function( model ) { return new CalculusGrapherScreenView( model ); },
-      { backgroundColor: 'white' }
+      { backgroundColorProperty: new Property( Color.toColor( 'white' ) ) }
     );
   }
 
