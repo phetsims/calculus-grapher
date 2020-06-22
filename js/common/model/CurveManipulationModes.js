2675d7387bf8e88294ff1ac0606db5fea78201ec
Brandon Li <brandon.li820@icloud.com>
2020-06-22 16:33:59 -0600

    #17 add CurveManipulationModes enum

--- js/common/model/CurveManipulationModes.js
+++ js/common/model/CurveManipulationModes.js
@@ -1,1 +1,28 @@
 // Copyright 2020, University of Colorado Boulder
+
+/**
+ * Enumeration of the possible 'modes' of manipulating curves.
+ *
+ * @author Brandon Li
+ */
+
+import Enumeration from '../../../../phet-core/js/Enumeration.js';
+import calculusGrapher from '../../calculusGrapher.js';
+
+const CurveManipulationModes = Enumeration.byKeys( [
+
+  // TODO document this.
+  'HILL',
+  'LINE',
+  'PEDESTAL',
+  'PARABOLA',
+  'SINE',
+  'FREEFORM',
+  'TILT',
+  'OFFSET'
+
+] );
+
+calculusGrapher.register( 'CurveManipulationModes', CurveManipulationModes );
+export default CurveManipulationModes;
+
