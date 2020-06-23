cdc42d971590c34a44efb43a1868f6992e439a6a
Brandon Li <brandon.li820@icloud.com>
2020-06-23 16:13:37 -0600

    #17 Add CurvePoint model

--- js/common/model/CurvePoint.js
+++ js/common/model/CurvePoint.js
@@ -1,1 +1,141 @@
 // Copyright 2020, University of Colorado Boulder
+
+/**
+ * CurvePoint is a single mutable point of a curve at a given x-value.
+ *
+ * Each CurvePoint contains the following information:
+ *   - The corresponding y-value of the point.
+ *   - All of its saved y-values. Y-values are saved when the user manipulates the original Curve.
+ *   - Whether or not the point is defined. A point that isn't defined is a hole or a discontinuity in the curve.
+ *   - Whether or not the point is differentiable. For 'Calculus Grapher', the only scenario when points are not
+ *     differentiable occur when there is a cusp. See https://en.wikipedia.org/wiki/Cusp_(singularity).
+ *
+ * For the 'Calculus Grapher' simulation, CurvePoints are used inside of Curve and its subtypes to represent and map
+ * out the curve at a finite number of points inside of a interval. Thus, CurvePoints are created at the start of the
+ * sim and are mutated when the curve changes. CurvePoints are never disposed.
+ *
+ * @author Brandon Li
+ */
+
+import NumberProperty from '../../../../axon/js/NumberProperty.js';
+import merge from '../../../../phet-core/js/merge.js';
+import calculusGrapher from '../../calculusGrapher.js';
+import CalculusGrapherConstants from '../CalculusGrapherConstants.js';
+
+class CurvePoint {
+
+  /**
+   * @param {number} x - the x-coordinate of the point.
+   * @param {number|null} initialY - the initial y-coordinate of the point.
+   * @param {Object} [options]
+   */
+  constructor( x, initialY, options ) {
+    assert && assert( Number.isFinite( x ) && CalculusGrapherConstants.CURVE_X_RANGE.contains( x ), `invalid x: ${x}` );
+    assert && assert( initialY === null || typeof initialY === 'number', `invalid initialY: ${initialY}` );
+
+    options = merge( {
+
+      // {boolean} - indicates if the point is differentiable.
+      isDifferentiable: true
+
+    }, options );
+
+    //----------------------------------------------------------------------------------------
+
+    // @public (read-only) {number} - the x-coordinate of the point. This value cannot be mutated.
+    this.x = x;
+
+    // @public (read-only) {NumberProperty} - the y-coordinate of the point.
+    this.yProperty = new NumberProperty( initialY );
+
+    // @private {number[]} - an array of all of this Point's saved y-values.
+    this.savedYValues = [];
+
+    // @public {boolean} - indicates if the point is currently differentiable.
+    this.isDifferentiable = options.isDifferentiable;
+
+    // @private {boolean} - the initial differentiability of the point.
+    this.initialIsDifferentiable = options.isDifferentiable;
+  }
+
+  /**
+   * Resets this CurvePoint to its factory settings.
+   * @public
+   *
+   * Called when the reset-all button is pressed.
+   */
+  reset() {
+    this.yProperty.reset();
+    this.savedYValues = [];
+    this.isDifferentiable = this.initialIsDifferentiable;
+  }
+
+  /**
+   * Restarts this Ball. Called when the restart button is pressed.
+   * @public
+   *
+   * See https://github.com/phetsims/collision-lab/issues/76 for context on the differences between reset and restart.
+   */
+  restart() {
+    this.position = this.restartState.position;
+    this.velocity = this.restartState.velocity;
+    this.mass = this.restartState.mass;
+  }
+
+  /**
+   * Saves the current y-value of the Point for the next undoToLastSave() method.
+   * @public
+   *
+   * This method is invoked when the user manipulates the original Curve. When the undo button is pressed, the points
+   * of the original curve will be set to their last saved state.
+   */
+  save() {
+
+    // Save the current y-value of the CurvePoint.
+    this.savedYValues.push( this.y );
+  }
+
+  /**
+   * Sets the y-value of this CurvedPoint to their last saved state.
+   * @public
+   *
+   * This method is invoked when the undo button is pressed, which successively undo the last action.
+   */
+  undoToLastSave() {
+
+    // Set the y-value of this CurvedPoint to the last saved state. The y-value is removed from our savedYValues
+    // so the next undoToLastSave() call successively reverts to the state before this one.
+    this.y = this.savedYValues.pop();
+  }
+
+  //----------------------------------------------------------------------------------------
+
+  /**
+   * Gets the y-value of the CurvePoint.
+   * @public
+   *
+   * @returns {number}
+   */
+  get y() { return this.yProperty.value; }
+
+  /**
+   * Sets the y-value of the CurvePoint.
+   * @public
+   *
+   * @param {number} y
+   */
+  set y( y ) { this.yProperty.value = y; }
+
+  /**
+   * Debugging string for the CurvePoint.
+   * @public
+   *
+   * @returns {string}
+   */
+  toString() {
+    return `CurvePoint[ x: ${this.x}, y: ${this.y} ]`;
+  }
+}
+
+calculusGrapher.register( 'CurvePoint', CurvePoint );
+export default CurvePoint;
