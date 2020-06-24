b8a352145bdbb02409aa03d5f11b3a8579dcd98b
Brandon Li <brandon.li820@icloud.com>
2020-06-24 14:39:38 -0600

    Add CalculusGrapherUtils

--- js/common/CalculusGrapherUtils.js
+++ js/common/CalculusGrapherUtils.js
@@ -1,1 +1,73 @@
 // Copyright 2020, University of Colorado Boulder
+
+/**
+ * CalculusGrapherUtils is a collection of general utility functions used in this sim.
+ *
+ * @author Brandon Li
+ */
+
+import AssertUtils from '../../../phetcommon/js/AssertUtils.js';
+import collisionLab from '../collisionLab.js';
+
+const CalculusGrapherUtils = {
+
+  /**
+   * Iterates through an array in pairs, passing the current value and the previous value to the iterator function.
+   * For instance, forEachAdjacentPair( [ 1, 2, 3, 4 ], f ) would invoke f( 2, 1 ), f( 3, 2 ), and f( 4, 3 ).
+   * @public
+   *
+   * @param {*[]} array
+   * @param {function(value:*,previousValue:*)} iterator
+   */
+  forEachAdjacentPair( array, iterator ) {
+    assert && assert( Array.isArray( array ), `invalid array: ${array}` );
+    assert && assert( typeof iterator === 'function', `invalid iterator: ${iterator}` );
+
+    for ( let i = 1; i < array.length; i++ ) {
+      const value = array[ i ];
+      const previousValue = array[ i - 1 ];
+
+      iterator( value, previousValue );
+    }
+  },
+
+  /**
+   * Determines whether an array is strictly sorted in ascending order (inclusive).
+   * @public
+   *
+   * @param {number[]} array
+   * @returns {boolean}
+   */
+  isSorted( array ) {
+    assert && AssertUtils.assertArrayOf( array, 'number' );
+
+    // Flag that indicates if the array is sorted.
+    let isSorted = true;
+
+    CalculusGrapherUtils.forEachAdjacentPair( array, ( value, previousValue ) => {
+      if ( isSorted ) {
+        isSorted = ( value >= previousValue );
+      }
+    } );
+    return isSorted;
+  },
+
+  /**
+   * Determines whether an array is strictly sorted in ascending order (inclusive) by a criterion function that
+   * numerically ranks each element of the array. The criterion function is passed each element of the collection.
+   * @public
+   *
+   * @param {*[]} array
+   * @param {function(value:*):number} criterion
+   * @returns {boolean}
+   */
+  isSortedBy( array, criterion ) {
+    assert && assert( Array.isArray( array ), `invalid array: ${array}` );
+    assert && assert( typeof criterion === 'function', `invalid criterion: ${criterion}` );
+
+    return CalculusGrapherUtils.isSorted( array.map( criterion ) );
+  }
+};
+
+collisionLab.register( 'CalculusGrapherUtils', CalculusGrapherUtils );
+export default CalculusGrapherUtils;
--- js/common/model/DerivativeCurve.js
+++ js/common/model/DerivativeCurve.js
@@ -1,1 +1,48 @@
 // Copyright 2020, University of Colorado Boulder
+
+/**
+ * DerivativeCurve is a Curve sub-type for a curve that represents the derivative of a 'base' curve. It is used
+ * as both the first derivative and second derivative of the OriginalCurve.
+ *
+ * When any of the Points of the base Curve changes, the derivative Curve differentiates it and updates its Points.
+ * For a general background on differentiation, see https://en.wikipedia.org/wiki/Derivative#Rigorous_definition.
+ * Although derivatives formally consider the behavior of the curve from both sides, for this simulation
+ * DerivativeCurves only consider the left side of each Point.
+ *
+ * Like Curve, DerivativeCurve is created at the start and persist for the lifetime of the simulation. Links
+ * are left as-is and DerivativeCurves are never disposed.
+ *
+ * @author Brandon Li
+ */
+
+import Property from '../../../../axon/js/Property.js';
+import calculusGrapher from '../../calculusGrapher.js';
+import Curve from './Curve.js';
+
+class DerivativeCurve {
+
+  /**
+   * @param {Curve} baseCurve - the curve to differentiate to get the values of this DerivativeCurve.
+   */
+  constructor( baseCurve ) {
+    assert && assert( baseCurve instanceof Curve, `invalid baseCurve: ${baseCurve}` );
+
+    // Observe when any of the base Curve's Point's y-value changes and update this curve to represent the derivative of
+    // the base Curve. Multilink is never disposed since DerivativeCurves are never disposed.
+    Property.multilink( baseCurve.points.map( _.property( 'yProperty' ) ), () => {
+
+      // this.points.forEach
+
+      //         for ( var i:int = 1; i < this.nbrPoints; i++ ) {
+      //       this.deriv_arr[i] = (y_arr[i] - y_arr[i - 1]) / this.dx;
+      //   }
+      //   this.deriv_arr[0] = this.deriv_arr[1];
+
+    } );
+
+
+  }
+}
+
+calculusGrapher.register( 'DerivativeCurve', DerivativeCurve );
+export default DerivativeCurve;
