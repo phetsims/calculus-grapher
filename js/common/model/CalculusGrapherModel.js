c55947215930e0098c8c5499d1c4bb55316d4217
Brandon Li <brandon.li820@icloud.com>
2020-07-09 11:28:31 -0600

    #25

--- js/common/model/CalculusGrapherModel.js
+++ js/common/model/CalculusGrapherModel.js
@@ -8,8 +8,8 @@
 
 import Tandem from '../../../../tandem/js/Tandem.js';
 import calculusGrapher from '../../calculusGrapher.js';
-import DerivativeCurve from './DerivativeCurve.js';
-import IntegralCurve from './IntegralCurve.js';
+// import DerivativeCurve from './DerivativeCurve.js';
+// import IntegralCurve from './IntegralCurve.js';
 import OriginalCurve from './OriginalCurve.js';
 
 class CalculusGrapherModel {
@@ -23,9 +23,9 @@ class CalculusGrapherModel {
 
     // @public (read-only)
     this.originalCurve = new OriginalCurve();
-    this.derivativeCurve = new DerivativeCurve( this.originalCurve );
-    this.secondDerivativeCurve = new DerivativeCurve( this.derivativeCurve );
-    this.integralCurve = new IntegralCurve( this.originalCurve );
+    // this.derivativeCurve = new DerivativeCurve( this.originalCurve );
+    // this.secondDerivativeCurve = new DerivativeCurve( this.derivativeCurve );
+    // this.integralCurve = new IntegralCurve( this.originalCurve );
 
   }
 }
