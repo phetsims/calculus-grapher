74017e54a99944c9f80c9d635908bed6f4bfb26a
phet-dev <phetadmin@gmail.com>
2022-03-05 03:20:06 -0700

    update copyright dates from daily grunt work

--- js/common/view/CurveNode.js
+++ js/common/view/CurveNode.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * CurveNode is the view representation of a single Curve, which appears in all screens of the 'Calculus Grapher'
