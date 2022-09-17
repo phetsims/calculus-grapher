ae4b05b493098c47bfeb1e7e542e76aea0153385
Martin Veillette <martin.veillette@gmail.com>
2022-09-17 14:10:56 -0400

    rename .js to .ts (see #36)
    Signed-off-by: Martin Veillette <martin.veillette@gmail.com>

--- js/common/model/IntegralCurve.ts
+++ js/common/model/IntegralCurve.ts
@@ -1,7 +1,5 @@
 // Copyright 2020-2022, University of Colorado Boulder
 
-// @ts-nocheck
-
 /**
  * IntegralCurve is a Curve sub-type for the curve that represents the integral of the OriginalCurve. The OriginalCurve
  * is referenced as the 'base' Curve of the IntegralCurve.
@@ -22,19 +20,19 @@ import CalculusGrapherUtils from '../CalculusGrapherUtils.js';
 import Curve from './Curve.js';
 import OriginalCurve from './OriginalCurve.js';
 
-class IntegralCurve extends Curve {
+export default class IntegralCurve extends Curve {
+
+  // reference to the 'base' Curve that was passed-in.
+  private baseCurve: OriginalCurve;
+
 
   /**
-   * @param {OriginalCurve} baseCurve - the curve to integrate to get the values of this IntegralCurve.
+   * @param baseCurve - the curve to integrate to get the values of this IntegralCurve.
    */
-  constructor( baseCurve ) {
-    assert && assert( baseCurve instanceof OriginalCurve, `invalid baseCurve: ${baseCurve}` );
+  public constructor( baseCurve: OriginalCurve ) {
 
     super();
 
-    //----------------------------------------------------------------------------------------
-
-    // @private {OriginalCurve} - reference to the 'base' Curve that was passed-in.
     this.baseCurve = baseCurve;
 
     // Observe when the 'base' Curve changes and update this curve to represent the integral of the 'base' Curve.
@@ -47,19 +45,15 @@ class IntegralCurve extends Curve {
 
   /**
    * Resets the IntegralCurve. Ensures that IntegralCurve matches the 'base' Curve regardless of resetting order.
-   * @override
-   * @public
-   *
    * Called when the reset-all button is pressed.
    */
-  reset() {
+  public override reset(): void {
     super.reset();
     this.updateIntegral();
   }
 
   /**
    * Updates the y-values of the IntegralCurve to represent the integral of the 'base' Curve.
-   * @private
    *
    * Since each adjacent Point of the base curve is considered to be infinitesimally close to each other, the
    * trapezoidal area between each adjacent Point is also considered to be infinitesimally small. Thus, summing up all
@@ -69,7 +63,7 @@ class IntegralCurve extends Curve {
    * The IntegralCurve exists at all points since OriginalCurve is finite at all points, so we don't need to consider
    * non-differentiable or non-finite points of the 'base' curve.
    */
-  updateIntegral() {
+  private updateIntegral(): void {
 
     // Loop through each pair of adjacent Points of the base Curve.
     CalculusGrapherUtils.forEachAdjacentPair( this.baseCurve.points, ( point, previousPoint, index ) => {
@@ -80,6 +74,7 @@ class IntegralCurve extends Curve {
       const trapezoidalArea = ( point.y + previousPoint.y ) / 2 * ( point.x - previousPoint.x );
 
       // Add the trapezoidalArea to the previous y-value to get the y-value of the current Point.
+      // @ts-ignore
       this.points[ index ].y = this.points[ index - 1 ].y + trapezoidalArea;
 
       // Sanity check that verifies that the Integral exists at the current Point.
@@ -92,4 +87,3 @@ class IntegralCurve extends Curve {
 }
 
 calculusGrapher.register( 'IntegralCurve', IntegralCurve );
-export default IntegralCurve;
