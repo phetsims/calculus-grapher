9bf493d1dbee210d41798923b3cec8b5ec28e214
Martin Veillette <martin.veillette@gmail.com>
2022-09-17 16:33:39 -0400

    export CurveNodeOptions
    Signed-off-by: Martin Veillette <martin.veillette@gmail.com>

--- js/common/view/OriginalCurveNode.ts
+++ js/common/view/OriginalCurveNode.ts
@@ -1,5 +1,5 @@
 // Copyright 2020-2022, University of Colorado Boulder
-// @ts-nocheck
+
 /**
  * OriginalCurveNode is a CurveNode sub-type for the main curve that the user interacts with and manipulates, which then
  * triggers a change in the model OriginalCurve's points.
@@ -11,36 +11,40 @@
  */
 
 import { Shape } from '../../../../kite/js/imports.js';
-import merge from '../../../../phet-core/js/merge.js';
-import AssertUtils from '../../../../phetcommon/js/AssertUtils.js';
 import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import { DragListener } from '../../../../scenery/js/imports.js';
 import calculusGrapher from '../../calculusGrapher.js';
 import CurveManipulationMode from '../model/CurveManipulationMode.js';
 import OriginalCurve from '../model/OriginalCurve.js';
-import CurveNode from './CurveNode.js';
+import CurveNode, { CurveNodeOptions } from './CurveNode.js';
+import Property from '../../../../axon/js/Property.js';
+import optionize from '../../../../phet-core/js/optionize.js';
+// import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
+
+// import IntegralCurve from './IntegralCurve.js';
+
+type SelfOptions = {
+  pathOptions: {
+    cursor: string;
+  };
+};
+
+// Make numberOfAtoms optional. Note that it must be omitted, then made required.
+type OriginalCurveNodeOptions = SelfOptions;
 
 // constants
 const CURVE_DRAG_DILATION = 0.4; // in model units
 
-class OriginalCurveNode extends CurveNode {
+export default class OriginalCurveNode extends CurveNode {
 
-  /**
-   * @param {OriginalCurve} curve - the model Curve.
-   * @param {Property.<ModelViewTransform2>} modelViewTransformProperty
-   * @param {Object} [options]
-   */
-  constructor( curve, modelViewTransformProperty, options ) {
-    assert && assert( curve instanceof OriginalCurve, `invalid curve: ${curve}` );
-    assert && AssertUtils.assertPropertyOf( modelViewTransformProperty, ModelViewTransform2 );
+  public constructor( curve: OriginalCurve, modelViewTransformProperty: Property<ModelViewTransform2>, providedOptions: OriginalCurveNodeOptions ) {
 
-    options = merge( {
-
+    const options = optionize<OriginalCurveNodeOptions, SelfOptions, CurveNodeOptions>()( {
       // super-class options
       pathOptions: {
         cursor: 'pointer'
       }
-    }, options );
+    }, providedOptions );
 
     super( curve, modelViewTransformProperty, options );
 
@@ -88,35 +92,20 @@ class OriginalCurveNode extends CurveNode {
   }
 
   /**
-   * @override
-   * Forwards response to super-class, except this adds a dilation to the touch/mouse area of the Path.
-   * @public
-   */
-  updateCurveNode() {
-    super.updateCurveNode();
-
-    const dilatedPathShape = OriginalCurveNode.createDilatedCurvePath( this.curve );
-    const dilatedPathShapeView = this.modelViewTransformProperty.value.modelToViewShape( dilatedPathShape );
-
-    this.path.touchArea = dilatedPathShapeView;
-    this.path.mouseArea = dilatedPathShapeView;
-  }
-
-  /**
    * Creates a (rough) dilated shape for a Curve.
-   * @public
    *
-   * @param {OriginalCurve} curve - the model Curve.
-   * @returns {Shape} - in model units
+   * @param  curve - the model Curve.
+   * @returns shape in model units
    */
-  static createDilatedCurvePath( curve ) {
-    assert && assert( curve instanceof OriginalCurve, `invalid curve: ${curve}` );
+  public static createDilatedCurvePath( curve: OriginalCurve ): Shape {
 
+    // @ts-ignore
     const pathShape = new Shape().moveTo( curve.points[ 0 ].x, curve.points[ 0 ].y - CURVE_DRAG_DILATION );
 
     // Draw the curve shape slightly BELLOW the true y-value.
     curve.points.forEach( point => {
       if ( point.exists ) {
+        // @ts-ignore
         pathShape.lineTo( point.x, point.y - CURVE_DRAG_DILATION );
       }
     } );
@@ -124,13 +113,28 @@ class OriginalCurveNode extends CurveNode {
     // Draw the curve shape slightly ABOVE the true y-value.
     _.forEachRight( curve.points, point => {
       if ( point.exists ) {
+        // @ts-ignore
         pathShape.lineTo( point.x, point.y + CURVE_DRAG_DILATION );
       }
     } );
 
     return pathShape.close().makeImmutable();
   }
+
+  /**
+   * Forwards response to super-class, except this adds a dilation to the touch/mouse area of the Path.
+   */
+  public override updateCurveNode(): void {
+    super.updateCurveNode();
+
+    // @ts-ignore
+    const dilatedPathShape = OriginalCurveNode.createDilatedCurvePath( this.curve );
+    // @ts-ignore
+    const dilatedPathShapeView = this.modelViewTransformProperty.value.modelToViewShape( dilatedPathShape );
+
+    this.path.touchArea = dilatedPathShapeView;
+    this.path.mouseArea = dilatedPathShapeView;
+  }
 }
 
 calculusGrapher.register( 'OriginalCurveNode', OriginalCurveNode );
-export default OriginalCurveNode;
