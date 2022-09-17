c03c5959e6ea9a5953868c652ee52f6617fdb745
Martin Veillette <martin.veillette@gmail.com>
2022-09-17 14:26:11 -0400

    // @ts-nocheck (see #36)
    Signed-off-by: Martin Veillette <martin.veillette@gmail.com>

--- js/common/model/DerivativeCurve.js
+++ js/common/model/DerivativeCurve.js
@@ -1,5 +1,6 @@
 // Copyright 2020-2021, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * DerivativeCurve is a Curve sub-type for a curve that represents the derivative of a 'base' curve. It is used
  * as both the first derivative and second derivative of the OriginalCurve.
