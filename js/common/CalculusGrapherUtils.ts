2d07ea9e86ce7b5140a2126e0c34bd0c6648283d
Martin Veillette <martin.veillette@gmail.com>
2022-09-19 21:41:10 -0400

    remove unused utility methods (see #40)
    Signed-off-by: Martin Veillette <martin.veillette@gmail.com>

--- js/common/CalculusGrapherUtils.ts
+++ js/common/CalculusGrapherUtils.ts
@@ -6,8 +6,6 @@
  * @author Brandon Li
  */
 
-import Range from '../../../dot/js/Range.js';
-import AssertUtils from '../../../phetcommon/js/AssertUtils.js';
 import calculusGrapher from '../calculusGrapher.js';
 import CurvePoint from './model/CurvePoint.js';
 
@@ -51,50 +49,6 @@ const CalculusGrapherUtils = {
 
       iterator( previousValue, value, nextValue, i );
     }
-  },
-
-  /**
-   * Gets a Range that is scaled by the desired scalar value. For instance, multiplyRange( new Range( 1, 2 ), 2 ) would
-   * return Range( 2, 4 ).
-   *
-   * @param range - will not be mutated
-   * @param scalar - amount to scale the extreme of the Range
-   */
-  multiplyRange( range: Range, scalar: number ): Range {
-
-    return new Range( range.min * scalar, range.max * scalar );
-  },
-
-  /**
-   * Determines whether an array is strictly sorted in ascending order (inclusive).
-   *
-   */
-  isSorted( array: ( CurvePoint )[] ): boolean {
-    assert && AssertUtils.assertArrayOf( array, 'number' );
-
-    // Flag that indicates if the array is sorted.
-    let isSorted = true;
-
-    CalculusGrapherUtils.forEachAdjacentPair( array, ( value, previousValue ) => {
-      if ( isSorted ) {
-        // @ts-ignore
-        isSorted = ( value >= previousValue );
-      }
-    } );
-    return isSorted;
-  },
-
-  /**
-   * Determines whether an array is strictly sorted in ascending order (inclusive) by a criterion function that
-   * numerically ranks each element of the array. The criterion function is passed each element of the collection.
-   *
-   */
-  isSortedBy( array: ( CurvePoint | null )[], criterion: ( value: CurvePoint | null, index: number, array: ( CurvePoint | null )[] ) => boolean ): boolean {
-    assert && assert( Array.isArray( array ), `invalid array: ${array}` );
-    assert && assert( typeof criterion === 'function', `invalid criterion: ${criterion}` );
-
-    // @ts-ignore
-    return CalculusGrapherUtils.isSorted( array.map( criterion ) );
   }
 };
 
