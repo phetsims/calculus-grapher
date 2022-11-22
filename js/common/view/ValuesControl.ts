6c33a7dfd50936504152dfa3fa1e71887c9d9b76
Marla Schulz <marlaa.schulz@gmail.com>
2022-11-21 16:25:46 -0800

    Generalize PreferencesToggleSwitch for a variety of controls, see: https://github.com/phetsims/joist/issues/842

--- js/common/view/ValuesControl.ts
+++ js/common/view/ValuesControl.ts
@@ -15,6 +15,8 @@ import StrictOmit from '../../../../phet-core/js/types/StrictOmit.js';
 import { HBox, HBoxOptions, Text, TextOptions } from '../../../../scenery/js/imports.js';
 import calculusGrapher from '../../calculusGrapher.js';
 import CalculusGrapherStrings from '../../CalculusGrapherStrings.js';
+import ToggleSwitch, { ToggleSwitchOptions } from '../../../../sun/js/ToggleSwitch.js';
+import PreferencesDialogConstants from '../../../../joist/js/preferences/PreferencesDialogConstants.js';
 
 type SelfOptions = {
   textOptions?: StrictOmit<TextOptions, 'tandem'>;
@@ -38,11 +40,17 @@ export default class ValuesControl extends HBox {
       tandem: options.tandem.createTandem( 'labelText' )
     }, options.textOptions ) );
 
-    const toggleSwitch = new PreferencesToggleSwitch( valuesVisibleProperty, false, true, {
+    const toggleSwitch = new ToggleSwitch( valuesVisibleProperty, false, true, combineOptions<ToggleSwitchOptions>( {
       tandem: options.tandem.createTandem( 'toggleSwitch' )
+    }, PreferencesDialogConstants.TOGGLE_SWITCH_OPTIONS ) );
+
+    const preferencesControl = new PreferencesToggleSwitch( {
+      labelNode: labelText,
+      controlNode: toggleSwitch,
+      tandem: options.tandem.createTandem( 'preferencesControl' )
     } );
 
-    options.children = [ labelText, toggleSwitch ];
+    options.children = [ preferencesControl ];
 
     super( options );
 
