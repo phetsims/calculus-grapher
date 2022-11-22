fb6c25da080c46a8a20adaa836f58872051d5661
pixelzoom <cmalley@pixelzoom.com>
2022-11-22 07:37:52 -0700

    fix ValuesControl, https://github.com/phetsims/joist/issues/842

--- js/common/view/ValuesControl.ts
+++ js/common/view/ValuesControl.ts
@@ -8,51 +8,45 @@
  */
 
 import Property from '../../../../axon/js/Property.js';
-import PreferencesToggleSwitch from '../../../../joist/js/preferences/PreferencesToggleSwitch.js';
-import optionize, { combineOptions } from '../../../../phet-core/js/optionize.js';
+import PreferencesToggleSwitch, { PreferencesToggleSwitchOptions } from '../../../../joist/js/preferences/PreferencesToggleSwitch.js';
+import optionize from '../../../../phet-core/js/optionize.js';
 import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
 import StrictOmit from '../../../../phet-core/js/types/StrictOmit.js';
-import { HBox, HBoxOptions, Text, TextOptions } from '../../../../scenery/js/imports.js';
+import { Text, TextOptions } from '../../../../scenery/js/imports.js';
 import calculusGrapher from '../../calculusGrapher.js';
 import CalculusGrapherStrings from '../../CalculusGrapherStrings.js';
-import ToggleSwitch, { ToggleSwitchOptions } from '../../../../sun/js/ToggleSwitch.js';
+import ToggleSwitch from '../../../../sun/js/ToggleSwitch.js';
 import PreferencesDialogConstants from '../../../../joist/js/preferences/PreferencesDialogConstants.js';
 
 type SelfOptions = {
   textOptions?: StrictOmit<TextOptions, 'tandem'>;
 };
 
-type ValuesControlOptions = SelfOptions & PickRequired<HBoxOptions, 'tandem'>;
+type ValuesControlOptions = SelfOptions & PickRequired<PreferencesToggleSwitchOptions, 'tandem'>;
 
-export default class ValuesControl extends HBox {
+export default class ValuesControl extends PreferencesToggleSwitch {
 
   private readonly disposeValuesControl: () => void;
 
   public constructor( valuesVisibleProperty: Property<boolean>, providedOptions: ValuesControlOptions ) {
 
-    const options = optionize<ValuesControlOptions, StrictOmit<SelfOptions, 'textOptions'>, HBoxOptions>()( {
+    const options = optionize<ValuesControlOptions, StrictOmit<SelfOptions, 'textOptions'>, PreferencesToggleSwitchOptions>()( {
 
-      // HBoxOptions
-      spacing: 10
+      // PreferencesToggleSwitchOptions
+      labelSpacing: 10
     }, providedOptions );
 
-    const labelText = new Text( CalculusGrapherStrings.valuesStringProperty, combineOptions<TextOptions>( {
-      tandem: options.tandem.createTandem( 'labelText' )
-    }, options.textOptions ) );
+    const labelText = new Text( CalculusGrapherStrings.valuesStringProperty, options.textOptions );
+    options.labelNode = labelText;
 
-    const toggleSwitch = new ToggleSwitch( valuesVisibleProperty, false, true, combineOptions<ToggleSwitchOptions>( {
-      tandem: options.tandem.createTandem( 'toggleSwitch' )
-    }, PreferencesDialogConstants.TOGGLE_SWITCH_OPTIONS ) );
-
-    const preferencesControl = new PreferencesToggleSwitch( {
-      labelNode: labelText,
-      controlNode: toggleSwitch,
-      tandem: options.tandem.createTandem( 'preferencesControl' )
-    } );
-
-    options.children = [ preferencesControl ];
+    const toggleSwitch = new ToggleSwitch( valuesVisibleProperty, false, true, PreferencesDialogConstants.TOGGLE_SWITCH_OPTIONS );
+    options.controlNode = toggleSwitch;
 
     super( options );
+
+    this.addLinkedElement( valuesVisibleProperty, {
+      tandem: options.tandem.createTandem( valuesVisibleProperty.tandem.name )
+    } );
 
     this.disposeValuesControl = () => {
       labelText.dispose();
