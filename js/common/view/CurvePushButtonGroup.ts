12771714cfe569815b524b6ac0f4f44fdd92652e
Martin Veillette <martin.veillette@gmail.com>
2022-12-01 08:43:54 -0500

    CurvePushButtonGroup extends vBox instead of Node
    Signed-off-by: Martin Veillette <martin.veillette@gmail.com>

--- js/common/view/CurvePushButtonGroup.ts
+++ js/common/view/CurvePushButtonGroup.ts
@@ -9,72 +9,79 @@
 
 import calculusGrapher from '../../calculusGrapher.js';
 import optionize, { combineOptions } from '../../../../phet-core/js/optionize.js';
-import { HBox, Node, NodeOptions, VBox } from '../../../../scenery/js/imports.js';
+import { HBox, VBox, VBoxOPtions } from '../../../../scenery/js/imports.js';
 import CalculusGrapherStrings from '../../CalculusGrapherStrings.js';
 import TextPushButton, { TextPushButtonOptions } from '../../../../sun/js/buttons/TextPushButton.js';
-import EraserButton from '../../../../scenery-phet/js/buttons/EraserButton.js';
-import UndoButton from '../../../../scenery-phet/js/buttons/UndoButton.js';
+import EraserButton, { EraserButtonOptions } from '../../../../scenery-phet/js/buttons/EraserButton.js';
+import UndoButton, { UndoButtonOptions } from '../../../../scenery-phet/js/buttons/UndoButton.js';
 import TransformedCurve from '../model/TransformedCurve.js';
 import PhetColorScheme from '../../../../scenery-phet/js/PhetColorScheme.js';
 
 type SelfOptions = {
   smoothButtonOptions?: TextPushButtonOptions;
+  undoButtonOptions?: UndoButtonOptions;
+  ereserButtonOptions?: EraserButtonOptions;
 };
 
-export type CurvePushButtonGroupOptions = SelfOptions & NodeOptions;
+export type CurvePushButtonGroupOptions = SelfOptions & VBoxOPtions;
 
-export default class CurvePushButtonGroup extends Node {
+export default class CurvePushButtonGroup extends VBox {
 
   public constructor( originalCurve: TransformedCurve,
                       providedOptions?: CurvePushButtonGroupOptions ) {
 
-    const options = optionize<CurvePushButtonGroupOptions, SelfOptions, NodeOptions>()( {
+    const options = optionize<CurvePushButtonGroupOptions, SelfOptions, VBoxOPtions>()( {
         smoothButtonOptions: {
           textNodeOptions: { fontWeight: 'bold' },
           baseColor: PhetColorScheme.BUTTON_YELLOW
+        },
+        undoButtonOptions: {
+          yMargin: 6,
+          iconOptions: { height: 13 }
+        },
+        eraserButtonOptions: {
+          iconWidth: 16,
+          xMargin: 10
         }
+
+        //VBoxOptions
+        spacing: 6
       },
       providedOptions
     );
 
     // Undo Button
-    const undoButton = new UndoButton( {
-      listener: () => originalCurve.undoToLastSave(),
-      iconOptions: { height: 13 },
-      tandem: options.tandem.createTandem( 'undoButton' )
-    } );
+    const undoButton = new UndoButton(
+      combineOptions<UndoButtonOptions>( {
+        listener: () => originalCurve.undoToLastSave(),
+        tandem: options.tandem.createTandem( 'undoButton' )
+      }, options.undoButtonOptions ) );
 
     // Eraser Button
-    const eraserButton = new EraserButton( {
-      listener: () => originalCurve.reset(),
-      iconWidth: 16,
-      xMargin: 10,
-      tandem: options.tandem.createTandem( 'resetButton' )
-    } );
+    const eraserButton = new EraserButton(
+      combineOptions<EraserButtonOptions>( {
+        listener: () => originalCurve.reset(),
+        tandem: options.tandem.createTandem( 'resetButton' )
+      }, options.eraserButtonOptions ) );
 
+    // Put the eraser and undo buttons side by side
     const hBox = new HBox( {
       spacing: 5,
       children: [ undoButton, eraserButton ]
     } );
 
-    // Smooth Button
+    // Smooth Button, with witdh matching the hbox
     const smoothButton = new TextPushButton( CalculusGrapherStrings.smoothStringProperty,
-      combineOptions<TextPushButtonOptions>(
-        {
+      combineOptions<TextPushButtonOptions>( {
           listener: () => originalCurve.smooth(),
-          yMargin: 6,
           maxWidth: hBox.width,
           minWidth: hBox.width,
           tandem: options.tandem.createTandem( 'smoothButton' )
         }, options.smoothButtonOptions
       ) );
 
-    const vBox = new VBox( {
-      spacing: 5,
-      children: [ smoothButton, hBox ]
-    } );
-
-    options.children = [ vBox ];
+    // vBox children
+    options.children = [ smoothButton, hBox ];
     super( options );
   }
 }
