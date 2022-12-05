041c9fca5f814660b8c92d1a979cf6d0c69f2363
pixelzoom <cmalley@pixelzoom.com>
2022-12-05 10:02:41 -0700

    some cleanup in GraphSetRadioButtonGroup, https://github.com/phetsims/calculus-grapher/issues/106

--- js/common/view/GraphSetRadioButtonGroup.ts
+++ js/common/view/GraphSetRadioButtonGroup.ts
@@ -9,16 +9,17 @@
 import RectangularRadioButtonGroup, { RectangularRadioButtonGroupItem, RectangularRadioButtonGroupOptions } from '../../../../sun/js/buttons/RectangularRadioButtonGroup.js';
 import calculusGrapher from '../../calculusGrapher.js';
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
-import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
 import Property from '../../../../axon/js/Property.js';
 import CurveLabelNode from './CurveLabelNode.js';
-import { Node } from '../../../../scenery/js/imports.js';
-import { GraphSet } from '../model/GraphType.js';
+import { GraphSet, GraphType } from '../model/GraphType.js';
 import CalculusGrapherColors from '../CalculusGrapherColors.js';
+import RectangularRadioButton from '../../../../sun/js/buttons/RectangularRadioButton.js';
 
 type SelfOptions = EmptySelfOptions;
 
 export type GraphSetRadioButtonGroupOptions = SelfOptions & RectangularRadioButtonGroupOptions;
+
+type GraphSetRadioButtonGroupItem = RectangularRadioButtonGroupItem<GraphSet>;
 
 export default class GraphSetRadioButtonGroup extends RectangularRadioButtonGroup<GraphSet> {
 
@@ -26,47 +27,37 @@ export default class GraphSetRadioButtonGroup extends RectangularRadioButtonGrou
                       graphSets: GraphSet[],
                       providedOptions: GraphSetRadioButtonGroupOptions ) {
 
-    const options = optionize<GraphSetRadioButtonGroupOptions, SelfOptions, RectangularRadioButtonGroupOptions>()(
-      {
+    const options = optionize<GraphSetRadioButtonGroupOptions, SelfOptions, RectangularRadioButtonGroupOptions>()( {
 
-        // RectangularRadioButtonGroupOptions
-        spacing: 5,
-        radioButtonOptions: {
-          baseColor: CalculusGrapherColors.panelFillProperty
-        }
-      }, providedOptions );
+      // RectangularRadioButtonGroupOptions
+      spacing: 5,
+      radioButtonOptions: {
+        baseColor: CalculusGrapherColors.panelFillProperty
+      }
+    }, providedOptions );
 
     assert && assert( graphSets[ 0 ][ 0 ] === 'integral', 'first button matches integral of graph set' );
 
     // Item for integral radio button
-    const integralRadioButtonItem = createItem(
-      new CurveLabelNode( { graphType: 'integral' } ),
-      graphSets[ 0 ], {
-        tandemName: 'integralRadioButton'
-      } );
+    const integralRadioButtonItem = GraphSetRadioButtonGroup.createItem( 'integral', graphSets[ 0 ] );
 
     // Item for derivative radio button
-    const derivativeRadioButtonItem = createItem(
-      new CurveLabelNode( { graphType: 'derivative' } ),
-      graphSets[ 1 ], {
-        tandemName: 'derivativeRadioButton'
-      } );
+    const derivativeRadioButtonItem = GraphSetRadioButtonGroup.createItem( 'derivative', graphSets[ 1 ] );
 
     super( graphsSelectionProperty, [ integralRadioButtonItem, derivativeRadioButtonItem ], options );
   }
-}
 
-type ItemOptions = PickRequired<RectangularRadioButtonGroupItem<GraphSet>, 'tandemName'>;
-
-function createItem( labelNode: Node,
-                     value: GraphSet,
-                     providedOptions: ItemOptions ): RectangularRadioButtonGroupItem<GraphSet> {
-
-  return {
-    createNode: () => labelNode,
-    value: value,
-    tandemName: providedOptions.tandemName
-  };
+  /**
+   * Creates an item for this radio button group.
+   */
+  public static createItem( graphType: GraphType, graphSet: GraphSet ): GraphSetRadioButtonGroupItem {
+    assert && assert( graphSet.includes( graphType ) );
+    return {
+      createNode: tandem => new CurveLabelNode( { graphType: graphType } ),
+      value: graphSet,
+      tandemName: `${graphType}${RectangularRadioButton.TANDEM_NAME_SUFFIX}`
+    };
+  }
 }
 
 calculusGrapher.register( 'GraphSetRadioButtonGroup', GraphSetRadioButtonGroup );
