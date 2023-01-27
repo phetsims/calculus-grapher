6481ee62ab984fd3201dfade31ff20c1bbcb52b5
Martin Veillette <martin.veillette@gmail.com>
2023-01-27 12:25:19 -0600

    more functions
    Signed-off-by: Martin Veillette <martin.veillette@gmail.com>

--- js/common/model/PresetFunctions.ts
+++ js/common/model/PresetFunctions.ts
@@ -18,7 +18,6 @@ const CENTER_X = X_RANGE.getCenter();
 const MIN_X = X_RANGE.min;
 const PI = Math.PI;
 const A = CalculusGrapherConstants.TYPICAL_Y;
-const SMALL_X = 0.1;
 const SPACING_X = WIDTH / ( CalculusGrapherQueryParameters.numberOfPoints - 1 );
 
 export type PresetFunction = {
@@ -32,17 +31,18 @@ export type PresetFunction = {
 const MathFunctions: MathFunction[] =
   [ x => A * Math.sin( 2 * PI * 5 * x / WIDTH ),
     x => A * Math.sin( 2 * PI * 20 * x / WIDTH ),
-    x => A / ( ( x - CENTER_X ) ** 2 + SMALL_X ** 2 ),
-    x => A * Math.cos( 1 / 10 * ( x - CENTER_X ) ** 2 ),
+    x => A * Math.sin( 2 * PI * 20 * x / WIDTH * Math.sin( 1 / 10 * ( x - CENTER_X ) ** 2 ) ),
+    x => A * Math.cos( ( x - CENTER_X ) ** 2 ),
     x => ( 1 / 10 * ( x - CENTER_X ) ** 2 % A ),
     x => A * Math.floor( A / 2 * Math.sin( 2 * PI * 5 * x / WIDTH ) )
   ];
 
 const PresetFunctions: PresetFunction[] =
   [ { mathFunction: MathFunctions[ 0 ] },
-    { mathFunction: MathFunctions[ 1 ], xPositions: getSpacedArray( 0.6 ) },
+    { mathFunction: MathFunctions[ 1 ], xPositions: getSpacedArray( 0.66 ) },
     { mathFunction: MathFunctions[ 1 ] },
     { mathFunction: MathFunctions[ 2 ] },
+    { mathFunction: MathFunctions[ 2 ], xPositions: getSpacedArray( 1.5 ) },
     { mathFunction: MathFunctions[ 3 ] },
     { mathFunction: MathFunctions[ 3 ], xPositions: getSpacedArray( 1 ) },
     { mathFunction: MathFunctions[ 4 ] },
