e1ccff7a5b6ae7b8405a9a9baa90fcb1e3a54a6e
phet-dev <phetadmin@gmail.com>
2023-01-28 03:58:37 -0700

    update copyright dates from daily grunt work

--- js/common/model/CalculusGrapherPreferences.ts
+++ js/common/model/CalculusGrapherPreferences.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * CalculusGrapherPreferences is the model for sim-specific preferences, accessed via the Preferences dialog.
