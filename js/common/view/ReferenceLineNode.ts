d2a4c78fb7c875b402bed102d19008e1bb007176
pixelzoom <cmalley@pixelzoom.com>
2023-03-05 19:40:54 -0700

    delete TODO, add doc, https://github.com/phetsims/calculus-grapher/issues/207

--- js/common/view/ReferenceLineNode.ts
+++ js/common/view/ReferenceLineNode.ts
@@ -37,8 +37,7 @@ export default class ReferenceLineNode extends ScrubberNode {
       lineStroke: CalculusGrapherColors.referenceLineStrokeProperty,
       lineDash: [], // solid line
 
-      //TODO https://github.com/phetsims/calculus-grapher/issues/207 is this still necessary?
-      // This is a hack to keep referenceLineNode.visibleProperty from showing up in Studio.
+      // This is a hack to keep referenceLineNode.visibleProperty from linking to referenceLine.visibleProperty in Studio.
       visibleProperty: new DerivedProperty( [ referenceLine.visibleProperty ], visible => visible ),
       tandem: tandem
     } );
