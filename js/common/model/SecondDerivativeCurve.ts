9c2c617bfff1104112bbc519d7dd975e99c15640
Martin Veillette <martin.veillette@gmail.com>
2023-03-14 13:48:20 -0400

    Replace base curve by originalCurve (see https://github.com/phetsims/calculus-grapher/issues/275)
    Signed-off-by: Martin Veillette <martin.veillette@gmail.com>

--- js/common/model/SecondDerivativeCurve.ts
+++ js/common/model/SecondDerivativeCurve.ts
@@ -1,9 +1,10 @@
 // Copyright 2023, University of Colorado Boulder
 
 /**
- * SecondDerivative is a Curve subclass for a curve that represents the second derivative of a 'base' curve.
+ * SecondDerivative is a Curve subclass for a curve that represents the second derivative of a Curve.
+ * It is used to evaluate the second derivative of the originalCurve.
  *
- * SecondDerivatives' main responsibility is to observe when the 'base' Curve changes and differentiates it and update
+ * SecondDerivatives' main responsibility is to observe when the originalCurve changes and differentiates it and update
  * the Points of the second derivative.
  *
  * Like Curve, SecondDerivative is created at the start and persists for the lifetime of the simulation. Links
@@ -18,70 +19,69 @@ import Curve from './Curve.js';
 
 export default class SecondDerivativeCurve extends Curve {
 
-  // Reference to the 'base' Curve that was passed-in.
-  private readonly baseCurve: Curve;
+  // Reference to the originalCurve that was passed-in.
+  private readonly originalCurve: Curve;
 
   /**
-   * @param baseCurve - the curve to differentiate to get the values of this SecondDerivative
+   * @param originalCurve - the curve to differentiate to get the values of this SecondDerivative
    * @param tandem
    */
-  public constructor( baseCurve: Curve, tandem: Tandem ) {
+  public constructor( originalCurve: Curve, tandem: Tandem ) {
 
     super( {
 
       // CurveOptions
-      xRange: baseCurve.xRange,
-      numberOfPoints: baseCurve.numberOfPoints,
+      xRange: originalCurve.xRange,
+      numberOfPoints: originalCurve.numberOfPoints,
       tandem: tandem
     } );
 
-    this.baseCurve = baseCurve;
+    this.originalCurve = originalCurve;
 
-    // Observes when the 'base' Curve changes and update this curve to represent the second derivative of the 'base' Curve.
+    // Observes when the originalCurve changes and update this curve to represent the second derivative of the originalCurve.
     // Listener is never removed since SecondDerivative is never disposed.
-    baseCurve.curveChangedEmitter.addListener( this.updateSecondDerivative.bind( this ) );
+    originalCurve.curveChangedEmitter.addListener( this.updateSecondDerivative.bind( this ) );
 
-    // Makes the initial call to updateSecondDerivative() to match the 'base' Curve upon initialization.
+    // Makes the initial call to updateSecondDerivative() to match the originalCurve upon initialization.
     this.updateSecondDerivative();
   }
 
   /**
-   * Updates the y-values of the SecondDerivative to represent the derivative of the 'base' Curve.
+   * Updates the y-values of the SecondDerivative to represent the derivative of the originalCurve.
    *
    * To update the second derivative, we (1) assume that the points are smooth,
    * and evaluate the second derivative using the standard finite difference algorithm.
    * For points that are not smooth, we correct for the wrong assumption, by assigning
    * their second derivative to a smooth point directly next to it.
-   *
    */
   private updateSecondDerivative(): void {
 
     // Convenience variables
-    const basePoints = this.baseCurve.points;
-    const length = basePoints.length;
+    const originalPoints = this.originalCurve.points;
+    const length = originalPoints.length;
 
     for ( let index = 0; index < length; index++ ) {
 
-      // Is the base point smooth?
-      const isBasePointSmooth = basePoints[ index ].pointType === 'smooth';
+      // Is the original point smooth?
+      const isOriginalPointSmooth = originalPoints[ index ].pointType === 'smooth';
 
-      // The point type is the same as the base point, unless the base point is not smooth, in which case it must be discontinuous
-      this.points[ index ].pointType = isBasePointSmooth ? 'smooth' : 'discontinuous';
+      // The point type is the same as the original point, unless the original point is not smooth, in which case it must be discontinuous
+      this.points[ index ].pointType = isOriginalPointSmooth ? 'smooth' : 'discontinuous';
 
       // We exclude the first and last point. They will be dealt with later
       if ( index !== 0 && index !== length - 1 ) {
-        const previousPoint = basePoints[ index - 1 ];
-        const point = basePoints[ index ];
-        const nextPoint = basePoints[ index + 1 ];
+        const previousPoint = originalPoints[ index - 1 ];
+        const point = originalPoints[ index ];
+        const nextPoint = originalPoints[ index + 1 ];
 
-        // Determine the second derivative using the naive assumption that all points are smooth. We will handle exceptions later
+        // Determine the second derivative using the naive assumption that all original points are smooth. We will handle exceptions later
         this.points[ index ].y = ( point.getSlope( nextPoint ) - point.getSlope( previousPoint ) ) / ( 2 * this.deltaX );
       }
     }
 
     // Handle the y value of the first and last point
-    this.points[ 0 ].y = ( basePoints[ 1 ].pointType === 'smooth' ) ? this.points[ 1 ].y : 0;
-    this.points[ length - 1 ].y = ( basePoints[ length - 2 ].pointType === 'smooth' ) ? this.points[ length - 2 ].y : 0;
+    this.points[ 0 ].y = ( originalPoints[ 1 ].pointType === 'smooth' ) ? this.points[ 1 ].y : 0;
+    this.points[ length - 1 ].y = ( originalPoints[ length - 2 ].pointType === 'smooth' ) ? this.points[ length - 2 ].y : 0;
 
 
     // Reiterate over points but this time taking into account the point type
