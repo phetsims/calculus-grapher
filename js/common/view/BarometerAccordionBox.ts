43cfdd555dd9fb3b0f266e24b1e0a01bf4b2a705
Sam Reid <reids@colorado.edu>
2023-09-01 19:22:08 -0600

    Rename variables and attributes, disable lint rule, see https://github.com/phetsims/phet-io/issues/1947

--- js/common/view/BarometerAccordionBox.ts
+++ js/common/view/BarometerAccordionBox.ts
@@ -98,6 +98,7 @@ export default class BarometerAccordionBox extends AccordionBox {
 
     const chartTransform = new ChartTransform( options.chartTransformOptions );
 
+    // eslint-disable-next-line tandem-name-should-match
     options.titleNode = new RichText( labelStringProperty, {
       font: CalculusGrapherConstants.ACCORDION_BOX_FONT,
       maxWidth: 80,
