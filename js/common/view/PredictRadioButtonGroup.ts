d8c08128da4d2cc11a14a49f07c74d9cbbde35c2
phet-dev <phetadmin@gmail.com>
2024-01-17 03:10:45 -0700

    update copyright dates from daily grunt work

--- js/common/view/PredictRadioButtonGroup.ts
+++ js/common/view/PredictRadioButtonGroup.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * PredictRadioButtonGroup is the radio button group for choosing either the original or predict curve.
