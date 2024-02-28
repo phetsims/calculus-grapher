10c3dd129d32c5b6b6428c0426d9d738231e474b
phet-dev <phetadmin@gmail.com>
2024-02-28 03:13:21 -0700

    update copyright dates from daily grunt work

--- js/common/view/preferences/VariableControl.ts
+++ js/common/view/preferences/VariableControl.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * VariableControl is the control in the Preferences dialog for selecting the variable used in functions.
