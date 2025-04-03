ed28f271d8115cb79a8dff6ae2dc87d9d56cdcc4
phet-dev <phetadmin@gmail.com>
2025-04-03 03:16:18 -0600

    update copyright dates from daily grunt work

--- js/common/model/GraphSet.ts
+++ js/common/model/GraphSet.ts
@@ -1,4 +1,4 @@
-// Copyright 2023-2024, University of Colorado Boulder
+// Copyright 2023-2025, University of Colorado Boulder
 
 /**
  * GraphSet is an ordered set of GraphType. We need a serializable type for this so that we can maintain references
