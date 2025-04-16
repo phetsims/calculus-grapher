e188f15e18f9c4821154fc9c946f07334d8cd9f8
phet-dev <phetadmin@gmail.com>
2025-04-16 03:57:49 -0600

    update copyright dates from daily grunt work

--- js/common/model/TransformedCurve.ts
+++ js/common/model/TransformedCurve.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2024, University of Colorado Boulder
+// Copyright 2020-2025, University of Colorado Boulder
 
 /**
  * TransformedCurve is a Curve subclass for a curve that the user interacts with and manipulates, which then
