0e6450d755045bdf9a03f42b2d0f2e0cd9dbe29b
phet-dev <phetadmin@gmail.com>
2025-08-28 03:41:42 -0600

    update copyright dates from daily grunt work

--- js/common/view/TransformedCurveNode.ts
+++ js/common/view/TransformedCurveNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2024, University of Colorado Boulder
+// Copyright 2020-2025, University of Colorado Boulder
 
 /**
  * TransformedCurveNode is a CurveNode subclass for the main curve that the user interacts with and manipulates,
