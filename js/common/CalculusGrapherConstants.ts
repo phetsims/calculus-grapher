151c9603574ac95f4c8bf4050034969a03e205bb
pixelzoom <cmalley@pixelzoom.com>
2025-10-30 16:19:24 -0600

    convert CalculusGrapherConstants to class, https://github.com/phetsims/calculus-grapher/issues/347

--- js/common/CalculusGrapherConstants.ts
+++ js/common/CalculusGrapherConstants.ts
@@ -16,8 +16,6 @@ import Text from '../../../scenery/js/nodes/Text.js';
 import { CheckboxOptions } from '../../../sun/js/Checkbox.js';
 import calculusGrapher from '../calculusGrapher.js';
 
-const CONTROL_FONT = new PhetFont( 16 ); // for text on checkboxes, radio buttons, push buttons, etc.
-
 // CAREFUL: Although there was a deliberate effort to scale other quantities based on
 // CURVE_X_RANGE, changing the CURVE_X_RANGE should lead to manual changes
 // for the tick labels, tick marks, and lookup table for yZoomLevelProperty (GraphNode.Y_ZOOM_INFO).
@@ -31,90 +29,85 @@ const TYPICAL_Y = 0.1 * CURVE_X_LENGTH;
 // Typical area of original graph, used to set a scale of 'Net Signed Area' barometer
 const TYPICAL_AREA = CURVE_X_LENGTH * TYPICAL_Y;
 
-// Because plots are typically clipped to the ChartRectangle, we do not care about their bounds.
-// So as a performance optimization, use 'none' for the boundsMethod where we're clipping to ChartRectangle.
-// See Path.setBoundsMethod and https://github.com/phetsims/calculus-grapher/issues/210
-const PLOT_BOUNDS_METHOD: PathBoundsMethod = 'none';
-
-const CHECKBOX_OPTIONS: CheckboxOptions = {
-  boxWidth: new Text( 'A', { font: CONTROL_FONT } ).height,
-  touchAreaXDilation: 6,
-  touchAreaYDilation: 3,
-  mouseAreaXDilation: 6,
-  mouseAreaYDilation: 3
-};
-
-const CalculusGrapherConstants = {
+export default class CalculusGrapherConstants {
 
   // margins
-  SCREEN_VIEW_X_MARGIN: 25,
-  SCREEN_VIEW_Y_MARGIN: 10,
-  PANEL_X_MARGIN: 8,
-  PANEL_Y_MARGIN: 6,
-  GRAPH_X_MARGIN: 15,
-  GRAPH_Y_MARGIN: 10,
+  public static readonly SCREEN_VIEW_X_MARGIN = 25;
+  public static readonly SCREEN_VIEW_Y_MARGIN = 10;
+  public static readonly PANEL_X_MARGIN = 8;
+  public static readonly PANEL_Y_MARGIN = 6;
+  public static readonly GRAPH_X_MARGIN = 15;
+  public static readonly GRAPH_Y_MARGIN = 10;
 
   // width of ChartRectangle for all GraphNode instances, in view coordinates
-  CHART_RECTANGLE_WIDTH: 612,
+  public static readonly CHART_RECTANGLE_WIDTH = 612;
 
   // If we had a single GraphNode instance, this would be the height of its ChartRectangle, in view coordinates.
-  SINGLE_CHART_RECTANGLE_HEIGHT: 490,
+  public static readonly SINGLE_CHART_RECTANGLE_HEIGHT = 490;
 
   // range for curve x-axis
-  CURVE_X_RANGE: CURVE_X_RANGE,
+  public static readonly CURVE_X_RANGE = CURVE_X_RANGE;
 
   // A typical Y value: PARABOLA, and TRIANGLE are CurveManipulation modes that don't have an intrinsic width,
   // but a curvature and a slope. We use TYPICAL_Y as a roundabout way to assign a meaning to the curve width parameter:
   // the function has a width across the baseline, when the peak of the function is at TYPICAL_Y.
-  TYPICAL_Y: TYPICAL_Y,
+  public static readonly TYPICAL_Y = TYPICAL_Y;
 
   // range for barometers
-  SLOPE_OF_TANGENT_MODEL_RANGE: new Range( -8, 8 ),
-  NET_SIGNED_AREA_MODEL_RANGE: new Range( -0.8 * TYPICAL_AREA, 0.8 * TYPICAL_AREA ),
+  public static readonly SLOPE_OF_TANGENT_MODEL_RANGE = new Range( -8, 8 );
+  public static readonly NET_SIGNED_AREA_MODEL_RANGE = new Range( -0.8 * TYPICAL_AREA, 0.8 * TYPICAL_AREA );
 
   // width of curve (in the same unit as x-Range) for curve manipulations
-  CURVE_MANIPULATION_WIDTH_RANGE: new RangeWithValue(
+  public static readonly CURVE_MANIPULATION_WIDTH_RANGE = new RangeWithValue(
     0.1 * CURVE_X_LENGTH,
     0.5 * CURVE_X_LENGTH,
-    0.20 * CURVE_X_LENGTH ),
+    0.20 * CURVE_X_LENGTH );
 
   // model height associated with curveManipulationDisplay
-  CURVE_MANIPULATION_Y_RANGE: new Range( -2, 2 ),
+  public static readonly CURVE_MANIPULATION_Y_RANGE = new Range( -2, 2 );
 
   // maximum of undo actions (See https://github.com/phetsims/calculus-grapher/issues/64)
-  MAX_UNDO: 20,
+  public static readonly MAX_UNDO = 20;
 
   // fonts
-  GRAPH_LABEL_FONT: new PhetFont( 16 ), // wherever graph labels like f(x), f'(x), etc. appear
-  TICK_LABEL_FONT: new PhetFont( 8 ), // for tick labels on graphs
-  CONTROL_FONT: CONTROL_FONT, // for text on checkboxes, radio buttons, push buttons, etc.
-  ACCORDION_BOX_FONT: new PhetFont( 16 ), // for accordion box title and non-numerical text
-  ACCORDION_BOX_VALUE_FONT: new PhetFont( 12 ), // for numerical values in the Accordion box
-  LABELED_LINE_FONT: new PhetFont( 16 ),
-  POINT_LABEL_FONT: new PhetFont( 16 ),
-  PREFERENCES_LABEL_FONT: new PhetFont( {
+  public static readonly GRAPH_LABEL_FONT = new PhetFont( 16 ); // wherever graph labels like f(x), f'(x), etc. appear
+  public static readonly TICK_LABEL_FONT = new PhetFont( 8 ); // for tick labels on graphs
+  public static readonly CONTROL_FONT = new PhetFont( 16 ); // for text on checkboxes, radio buttons, push buttons, etc.
+  public static readonly ACCORDION_BOX_FONT = new PhetFont( 16 ); // for accordion box title and non-numerical text
+  public static readonly ACCORDION_BOX_VALUE_FONT = new PhetFont( 12 ); // for numerical values in the Accordion box
+  public static readonly LABELED_LINE_FONT = new PhetFont( 16 );
+  public static readonly POINT_LABEL_FONT = new PhetFont( 16 );
+  public static readonly PREFERENCES_LABEL_FONT = new PhetFont( {
     size: 16,
     weight: 'bold'
-  } ),
-  PREFERENCES_LABEL_MAX_WIDTH: 200,
-  PREFERENCES_DESCRIPTION_FONT: new PhetFont( 16 ),
+  } );
+  public static readonly PREFERENCES_LABEL_MAX_WIDTH = 200;
+  public static readonly PREFERENCES_DESCRIPTION_FONT = new PhetFont( 16 );
 
-  PREFERENCES_DESCRIPTION_LINE_WRAP: 325,
+  public static readonly PREFERENCES_DESCRIPTION_LINE_WRAP = 325;
 
-  CHECKBOX_OPTIONS: CHECKBOX_OPTIONS,
+  public static readonly CHECKBOX_OPTIONS: CheckboxOptions = {
+    boxWidth: new Text( 'A', { font: CalculusGrapherConstants.CONTROL_FONT } ).height,
+    touchAreaXDilation: 6,
+    touchAreaYDilation: 3,
+    mouseAreaXDilation: 6,
+    mouseAreaYDilation: 3
+  };
 
   // for Panels and AccordionBoxes
-  CORNER_RADIUS: 5,
+  public static readonly CORNER_RADIUS = 5;
 
   // PhET-iO ONLY constants
-  NUMBER_OF_LABELED_LINES: 6,
-  NUMBER_OF_LABELED_POINTS: 6,
+  public static readonly NUMBER_OF_LABELED_LINES = 6;
+  public static readonly NUMBER_OF_LABELED_POINTS = 6;
 
-  PLOT_BOUNDS_METHOD: PLOT_BOUNDS_METHOD,
+  // Because plots are typically clipped to the ChartRectangle, we do not care about their bounds.
+  // So as a performance optimization, use 'none' for the boundsMethod where we're clipping to ChartRectangle.
+  // See Path.setBoundsMethod and https://github.com/phetsims/calculus-grapher/issues/210
+  public static readonly PLOT_BOUNDS_METHOD: PathBoundsMethod = 'none';
 
   // radius of scrubber handles
-  SCRUBBER_RADIUS: 9
-};
+  public static readonly SCRUBBER_RADIUS = 9;
+}
 
 calculusGrapher.register( 'CalculusGrapherConstants', CalculusGrapherConstants );
-export default CalculusGrapherConstants;
