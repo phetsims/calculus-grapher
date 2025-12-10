c303fbf45bcf9be74f727c858f4f4fe8336d99ef
phet-dev <phetadmin@gmail.com>
2025-12-10 06:00:11 -0700

    grunt update  from daily grunt work

--- js/CalculusGrapherFluent.ts
+++ js/CalculusGrapherFluent.ts
@@ -5,10 +5,10 @@
 /* eslint-disable */
 /* @formatter:off */
 
+import type { FluentVariable } from '../../chipper/js/browser/FluentPattern.js';
+import FluentPattern from '../../chipper/js/browser/FluentPattern.js';
 import FluentConstant from '../../chipper/js/browser/FluentConstant.js';
 import FluentContainer from '../../chipper/js/browser/FluentContainer.js';
-import type {FluentVariable} from '../../chipper/js/browser/FluentPattern.js';
-import FluentPattern from '../../chipper/js/browser/FluentPattern.js';
 import calculusGrapher from './calculusGrapher.js';
 import CalculusGrapherStrings from './CalculusGrapherStrings.js';
 
