a980e642b97d92353c1d5d80b6f627f2256834da
phet-dev <phetadmin@gmail.com>
2026-01-06 03:53:10 -0700

    update copyright dates from daily grunt work

--- js/common/view/CurveManipulatorNode.ts
+++ js/common/view/CurveManipulatorNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * CurveManipulatorNode is a draggable point used to manipulate the curve.
