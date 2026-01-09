c243d26e075a740052cc2e285078b98359e293f5
phet-dev <phetadmin@gmail.com>
2026-01-09 03:33:04 -0700

    update copyright dates from daily grunt work

--- js/common/model/CurveManipulator.ts
+++ js/common/model/CurveManipulator.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * CurveManipulator is the model for a draggable point used to manipulate the original curve.
