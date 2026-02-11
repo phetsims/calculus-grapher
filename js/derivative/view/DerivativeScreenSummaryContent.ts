902dadc45bb85449fb43c9c88c141aa270592215
phet-dev <phetadmin@gmail.com>
2026-02-11 03:50:01 -0700

    update copyright dates from daily grunt work

--- js/derivative/view/DerivativeScreenSummaryContent.ts
+++ js/derivative/view/DerivativeScreenSummaryContent.ts
@@ -1,4 +1,4 @@
-// Copyright 2026, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * DerivativeScreenSummaryContent is the screen summary for the 'Derivative' screen.
--- js/lab/view/LabScreenSummaryContent.ts
+++ js/lab/view/LabScreenSummaryContent.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * LabScreenSummaryContent is the screen summary for the 'Lab' screen.
