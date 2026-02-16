21b6f88ffa3cfa041086156552f98e2ec3451e64
pixelzoom <cmalley@pixelzoom.com>
2026-02-16 10:42:27 -0700

    factor out start and update methods, https://github.com/phetsims/calculus-grapher/issues/370

--- js/common/view/CurveManipulatorDragListener.ts
+++ js/common/view/CurveManipulatorDragListener.ts
@@ -19,6 +19,7 @@ import SoundRichDragListener from '../../../../scenery-phet/js/SoundRichDragList
 import Tandem from '../../../../tandem/js/Tandem.js';
 import calculusGrapher from '../../calculusGrapher.js';
 import CurveManipulationType from '../model/CurveManipulationType.js';
+import CurveManipulator from '../model/CurveManipulator.js';
 import TransformedCurve from '../model/TransformedCurve.js';
 import CurveManipulatorNode from './CurveManipulatorNode.js';
 
@@ -28,83 +29,24 @@ const FREEFORM_MIN_DX = 0.1;
 
 export default class CurveManipulatorDragListener extends SoundRichDragListener {
 
+  // Constructor args used in methods.
+  private readonly curveManipulator: CurveManipulator;
+  private readonly transformedCurve: TransformedCurve;
+  private readonly curveManipulationModeProperty: TReadOnlyProperty<CurveManipulationType>;
+  private readonly curveManipulationWidthProperty: TReadOnlyProperty<number>;
+
+  // Variables to keep track of old model positions associated with the dragListener.
+  // Set them to null as no drag event has occurred yet.
+  // These are relevant only for CurveManipulationType.FREEFORM.
+  private penultimatePosition: Vector2 | null; // second to last position
+  private antepenultimatePosition: Vector2 | null; // third to last position
+
   public constructor( curveManipulatorNode: CurveManipulatorNode,
                       transformedCurve: TransformedCurve,
                       chartTransform: ChartTransform,
                       curveManipulationModeProperty: TReadOnlyProperty<CurveManipulationType>,
                       curveManipulationWidthProperty: TReadOnlyProperty<number>,
                       tandem: Tandem ) {
-
-    // Variables to keep track of old model positions associated with the dragListener.
-    // Set them to null as no drag event has occurred yet.
-    // These are relevant only for CurveManipulationType.FREEFORM.
-    let penultimatePosition: Vector2 | null = null; // second to last position
-    let antepenultimatePosition: Vector2 | null = null; // third to last position
-
-    const curveManipulator = curveManipulatorNode.curveManipulator;
-
-    // Update whichever curve is currently interactive.
-    const update = ( isEventFromPDOM: boolean, modelPosition: Vector2 ): void => {
-
-      // When the manipulator is first moved with the keyboard, disable the keyboard cue so
-      // that we do not see it until Reset All is pressed.
-      if ( isEventFromPDOM ) {
-        curveManipulator.keyboardCueEnabledProperty.value = false;
-      }
-
-      if ( !isEventFromPDOM || curveManipulator.keyboardModeProperty.value === 'grabbed' ) {
-        if ( curveManipulationModeProperty.value === CurveManipulationType.FREEFORM ) {
-
-          // Do not update the curve model if the drag points in (FREEFORM mode) are too close to each another,
-          // to prevent noise in the derivative (see https://github.com/phetsims/calculus-grapher/issues/297 )
-          if ( penultimatePosition === null || Math.abs( modelPosition.x - penultimatePosition.x ) > FREEFORM_MIN_DX ) {
-
-            // Update the curve.
-            transformedCurve.manipulateCurve(
-              curveManipulationModeProperty.value,
-              curveManipulationWidthProperty.value,
-              modelPosition,
-              penultimatePosition,
-              antepenultimatePosition );
-
-            // Update (model) antepenultimatePosition and penultimatePosition
-            antepenultimatePosition = penultimatePosition;
-            penultimatePosition = modelPosition;
-
-            // Move to the new position.
-            curveManipulator.positionProperty.value = modelPosition;
-          }
-        }
-        else { // For any CurveManipulationType other than FREEFORM...
-
-          // Update the curve.
-          transformedCurve.manipulateCurve(
-            curveManipulationModeProperty.value,
-            curveManipulationWidthProperty.value,
-            modelPosition );
-
-          // Move to the new position.
-          curveManipulator.positionProperty.value = modelPosition;
-        }
-      }
-      else {
-
-        // Move the manipulator to the new position without modifying the curve.
-        curveManipulator.positionProperty.value = modelPosition;
-      }
-    };
-
-    const start = ( isFromPDOM: boolean ) => {
-      // Save the current values of the CurvePoints for the next undo() call.
-      // This must be called once at the start of dragging (and not on each micro drag-position change).
-      transformedCurve.save();
-
-      // Set the previous last positions to null, since it is a new drag.
-      antepenultimatePosition = null;
-      penultimatePosition = null;
-
-      update( isFromPDOM, curveManipulator.positionProperty.value );
-    };
 
     super( {
 
@@ -116,7 +58,7 @@ export default class CurveManipulatorDragListener extends SoundRichDragListener 
       ),
 
       // Position in model coordinates.
-      positionProperty: curveManipulator.positionProperty,
+      positionProperty: curveManipulatorNode.curveManipulator.positionProperty,
 
       // Drag bounds in model coordinates.
       dragBoundsProperty: new Property( new Bounds2(
@@ -132,9 +74,9 @@ export default class CurveManipulatorDragListener extends SoundRichDragListener 
         shiftDragSpeed: 30
       },
 
-      start: ( event, listener ) => start( event.isFromPDOM() ),
+      start: ( event, listener ) => this.start( event.isFromPDOM() ),
 
-      drag: ( event, listener ) => update( event.isFromPDOM(), curveManipulator.positionProperty.value ),
+      drag: ( event, listener ) => this.update( event.isFromPDOM() ),
 
       end: ( event, listener ) => {
         // Fuzzing does not provide an event.
@@ -144,12 +86,92 @@ export default class CurveManipulatorDragListener extends SoundRichDragListener 
       tandem: tandem
     } );
 
+    this.curveManipulator = curveManipulatorNode.curveManipulator;
+    this.transformedCurve = transformedCurve;
+    this.curveManipulationModeProperty = curveManipulationModeProperty;
+    this.curveManipulationWidthProperty = curveManipulationWidthProperty;
+    this.penultimatePosition = null;
+    this.antepenultimatePosition = null;
+
     // When the curve is grabbed using the keyboard, treat this as the start of a drag.
-    curveManipulator.keyboardModeProperty.link( keyboardMode => {
+    this.curveManipulator.keyboardModeProperty.link( keyboardMode => {
       if ( keyboardMode === 'grabbed' ) {
-        start( true /* isFromPDOM */ );
+        this.start( true /* isFromPDOM */ );
       }
     } );
+  }
+
+  /**
+   * Starts a drag cycle for all forms of input.
+   */
+  private start( isFromPDOM: boolean ): void {
+
+    // Save the current values of the CurvePoints for the next undo() call.
+    // This must be called once at the start of dragging (and not on each micro drag-position change).
+    this.transformedCurve.save();
+
+    // Set the previous last positions to null, since it is a new drag.
+    this.antepenultimatePosition = null;
+    this.penultimatePosition = null;
+
+    this.update( isFromPDOM );
+  }
+
+  /**
+   * Updates the curve model at the manipulator's current position, applying the selected manipulation type and width
+   * as is appropriate.
+   */
+  private update( isEventFromPDOM: boolean ): void {
+
+    // When the manipulator is first moved with the keyboard, disable the keyboard cue so
+    // that we do not see it until Reset All is pressed.
+    if ( isEventFromPDOM ) {
+      this.curveManipulator.keyboardCueEnabledProperty.value = false;
+    }
+
+    const modelPosition = this.curveManipulator.positionProperty.value;
+
+    if ( !isEventFromPDOM || this.curveManipulator.keyboardModeProperty.value === 'grabbed' ) {
+
+      if ( this.curveManipulationModeProperty.value === CurveManipulationType.FREEFORM ) {
+
+        // Do not update the curve model if the drag points in (FREEFORM mode) are too close to each another,
+        // to prevent noise in the derivative (see https://github.com/phetsims/calculus-grapher/issues/297 )
+        if ( this.penultimatePosition === null || Math.abs( modelPosition.x - this.penultimatePosition.x ) > FREEFORM_MIN_DX ) {
+
+          // Update the curve.
+          this.transformedCurve.manipulateCurve(
+            this.curveManipulationModeProperty.value,
+            this.curveManipulationWidthProperty.value,
+            modelPosition,
+            this.penultimatePosition,
+            this.antepenultimatePosition );
+
+          // Update (model) antepenultimatePosition and penultimatePosition
+          this.antepenultimatePosition = this.penultimatePosition;
+          this.penultimatePosition = modelPosition;
+
+          // Move to the new position.
+          this.curveManipulator.positionProperty.value = modelPosition;
+        }
+      }
+      else { // For any CurveManipulationType other than FREEFORM...
+
+        // Update the curve.
+        this.transformedCurve.manipulateCurve(
+          this.curveManipulationModeProperty.value,
+          this.curveManipulationWidthProperty.value,
+          modelPosition );
+
+        // Move to the new position.
+        this.curveManipulator.positionProperty.value = modelPosition;
+      }
+    }
+    else {
+
+      // Move the manipulator to the new position without modifying the curve.
+      this.curveManipulator.positionProperty.value = modelPosition;
+    }
   }
 }
 
