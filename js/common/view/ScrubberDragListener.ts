65787061de60778d4bc8a11a78a217f57ae16146
pixelzoom <cmalley@pixelzoom.com>
2026-02-23 13:18:14 -0700

    factor out ScrubberDragListener, https://github.com/phetsims/calculus-grapher/issues/387

---
+++ js/common/view/ScrubberDragListener.ts
@@ -0,0 +1,64 @@
+// Copyright 2026, University of Colorado Boulder
+
+/**
+ * ScrubberDragListener is the drag listener for scrubbers (Reference Line, Tangent, Area Under Curve).
+ * It handles both pointer and keyboard dragging, with sound feedback. The scrubber is constrained to
+ * dragging horizontally within the range of the chart.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+
+import Property from '../../../../axon/js/Property.js';
+import TRangedProperty from '../../../../axon/js/TRangedProperty.js';
+import ChartTransform from '../../../../bamboo/js/ChartTransform.js';
+import Bounds2 from '../../../../dot/js/Bounds2.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
+import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
+import SoundRichDragListener from '../../../../scenery-phet/js/SoundRichDragListener.js';
+import Tandem from '../../../../tandem/js/Tandem.js';
+import calculusGrapher from '../../calculusGrapher.js';
+import ScrubberNode from './ScrubberNode.js';
+
+export default class ScrubberDragListener extends SoundRichDragListener {
+
+  public constructor( scrubberNode: ScrubberNode,
+                      xProperty: TRangedProperty,
+                      positionProperty: Property<Vector2>,
+                      chartTransform: ChartTransform,
+                      tandem: Tandem ) {
+
+    // Constrain to the chart range.
+    const dragBoundsProperty = new Property( new Bounds2(
+      chartTransform.modelXRange.min, chartTransform.modelYRange.min,
+      chartTransform.modelXRange.max, chartTransform.modelYRange.max ) );
+
+    super( {
+
+      // Synthesize a ModelViewTransform2 from the ChartTransform.
+      transform: ModelViewTransform2.createOffsetXYScaleMapping(
+        new Vector2( 0, chartTransform.viewHeight / 2 ), // offset of the origin in view coordinates
+        chartTransform.viewWidth / chartTransform.modelXRange.getLength(), // xScale, model to view
+        -( chartTransform.viewHeight / chartTransform.modelYRange.getLength() ) // yScale, model to view
+      ),
+
+      positionProperty: positionProperty,
+      dragBoundsProperty: dragBoundsProperty,
+
+      keyboardDragListenerOptions: {
+        dragDelta: chartTransform.modelToViewDeltaX( 0.1 ),
+        shiftDragDelta: chartTransform.modelToViewDeltaX( 0.01 ),
+        moveOnHoldInterval: 50
+      },
+
+      drag: ( event, listener ) => {
+        xProperty.value = positionProperty.value.x;
+      },
+
+      end: () => scrubberNode.doAccessibleObjectResponse(),
+
+      tandem: tandem
+    } );
+  }
+}
+
+calculusGrapher.register( 'ScrubberDragListener', ScrubberDragListener );
--- js/common/view/ScrubberHandleNode.ts
+++ js/common/view/ScrubberHandleNode.ts
@@ -10,20 +10,18 @@
 import Property from '../../../../axon/js/Property.js';
 import TRangedProperty from '../../../../axon/js/TRangedProperty.js';
 import ChartTransform from '../../../../bamboo/js/ChartTransform.js';
-import Bounds2 from '../../../../dot/js/Bounds2.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import Shape from '../../../../kite/js/Shape.js';
 import { optionize4 } from '../../../../phet-core/js/optionize.js';
 import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
-import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import AccessibleDraggableOptions from '../../../../scenery-phet/js/accessibility/grab-drag/AccessibleDraggableOptions.js';
 import ShadedSphereNode, { ShadedSphereNodeOptions } from '../../../../scenery-phet/js/ShadedSphereNode.js';
-import SoundRichDragListener from '../../../../scenery-phet/js/SoundRichDragListener.js';
 import InteractiveHighlighting from '../../../../scenery/js/accessibility/voicing/InteractiveHighlighting.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import TColor from '../../../../scenery/js/util/TColor.js';
 import calculusGrapher from '../../calculusGrapher.js';
 import CalculusGrapherConstants from '../CalculusGrapherConstants.js';
+import ScrubberDragListener from './ScrubberDragListener.js';
 import { ScrubberKeyboardListener } from './ScrubberKeyboardListener.js';
 import ScrubberNode from './ScrubberNode.js';
 
@@ -78,38 +76,8 @@ export default class ScrubberHandleNode extends InteractiveHighlighting( ShadedS
     // Initial position in model coordinates.
     const positionProperty = new Property( new Vector2( xProperty.value, chartTransform.viewToModelY( this.y ) ) );
 
-    // Drag bounds in model coordinates.
-    const dragBoundsProperty = new Property( new Bounds2(
-      chartTransform.modelXRange.min, chartTransform.modelYRange.min,
-      chartTransform.modelXRange.max, chartTransform.modelYRange.max ) );
-
     // As the handle is dragged, change xProperty.
-    this.addInputListener( new SoundRichDragListener( {
-
-      // Synthesize a ModelViewTransform2 from the ChartTransform.
-      transform: ModelViewTransform2.createOffsetXYScaleMapping(
-        new Vector2( 0, chartTransform.viewHeight / 2 ), // offset of the origin in view coordinates
-        chartTransform.viewWidth / chartTransform.modelXRange.getLength(), // xScale, model to view
-        -( chartTransform.viewHeight / chartTransform.modelYRange.getLength() ) // yScale, model to view
-      ),
-
-      positionProperty: positionProperty,
-      dragBoundsProperty: dragBoundsProperty,
-
-      keyboardDragListenerOptions: {
-        dragDelta: chartTransform.modelToViewDeltaX( 0.1 ),
-        shiftDragDelta: chartTransform.modelToViewDeltaX( 0.01 ),
-        moveOnHoldInterval: 50
-      },
-
-      drag: ( event, listener ) => {
-        xProperty.value = positionProperty.value.x;
-      },
-
-      end: () => scrubberNode.doAccessibleObjectResponse(),
-
-      tandem: options.tandem
-    } ) );
+    this.addInputListener( new ScrubberDragListener( scrubberNode, xProperty, positionProperty, chartTransform, options.tandem ) );
 
     // As xProperty changes, translate this Node.
     xProperty.link( x => {
